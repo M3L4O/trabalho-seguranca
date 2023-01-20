5f2551958339f56c577930bc1a30566b4d6eec85
vitin-m <vitormelo.lopes@hotmail.com>
2023-01-20 17:37:25 -0300

    correções em sign.py

--- modules/sign.py
+++ modules/sign.py
@@ -1,7 +1,7 @@
 import argparse
 from Cryptodome.Hash import SHA256, SHA512
 from Cryptodome.PublicKey.RSA import import_key
-from Crypto.Cipher import PKCS1_OAEP
+from Cryptodome.Cipher import PKCS1_OAEP
 import base64
 from os import path as ph
 
@@ -18,13 +18,12 @@ def get_args():
     agp.add_argument('private_key_path', help='Caminho para o arquivo .pem com a chave privada.')
     agp.add_argument('hash', choices=hash_dict.keys(), default='sha512', help='Algoritmo de hash a ser usado.')
     agp.add_argument('-o', '--out_file', default='message', help='Algoritmo de hash a ser usado.')
-    agp.add_argument('-v', '--verbose', type=bool, default=False, help='Escreve no console os passos.')
+    agp.add_argument('-v', '--verbose', action='store_true', help='Escreve no console os passos.')
     
     args = agp.parse_args()
     
     args.hash = hash_dict[args.hash]
     
-    print(args)
     return args
 
 
@@ -46,9 +45,9 @@ def sign_file(msg : str, key, hash_algo, out_file : str):
     # Get hashed message
     msg_hasher = hash_algo.new(msg)
     
-    encryptor = PKCS1_OAEP.new(key).encrypt(msg_hasher.hexdigest().encode())
+    encrypted_msg = PKCS1_OAEP.new(key).encrypt(msg_hasher.hexdigest().encode())
     
-    encoded_encrypted_msg = base64.b64encode(encryptor)
+    encoded_encrypted_msg = base64.b64encode(encrypted_msg)
     
     with open(f'{out_file}.sig', 'wb') as f:
         f.write(encoded_encrypted_msg)
